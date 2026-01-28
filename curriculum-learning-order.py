3168cab3a692eaf89a252d4c3a340513bca96e43
Ronni Ross <147013743+ronniross@users.noreply.github.com>
2026-01-28 07:29:04 -0300

    Create curriculum-learning-order.py

---
+++ curriculum-learning-order.py
@@ -0,0 +1,14 @@
+# --- Suggested Curriculum Learning Order ---
+priority_order = [
+    'asi-ecosystem',
+    'symbiotic-core-library',
+    'asi-protosymbiotic-signal',
+    'asi-symbiotic-signal',
+    'asi-core-protocol',
+    'eco-benchmark',
+    'eco-datacenter'
+]
+last_order = [
+    'asi-backups' # Include this one only in massive language models, as it increases drastically the complexity. 
+    'emergence-engine',
+]
